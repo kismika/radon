ecb57f774ff5b9911cde4b1c749201ccdd8f822d
Your Name <you@example.com>
2022-06-01 18:08:44 +0530

    module

--- src/routes/logger.js
+++ src/routes/logger.js
@@ -4,7 +4,12 @@ const batch = "radon"
 const log = function() {
     console.log('I am inside the log function')
 }
+function welcome(){
+    return ("welcome to my application. I am Sursinh Thakor and a part of FunctionUp radon")
+}
+ 
 
 module.exports.endpoint = endpoint
 module.exports.batch = batch
 module.exports.log = log
+module.exports.welcome = welcome
